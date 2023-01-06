462e46d6c97384da44090a8d748c8a0659e679af
riufix <enguerrand.cousin@gmail.com>
2023-01-06 18:07:51 +0100

    TU_1erRendu
    aaa

--- TU Challenge/MyMathImplementation.cs
+++ TU Challenge/MyMathImplementation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,171 @@ namespace TU_Challenge
 {
     public class MyMathImplementation
     {
-        
+        internal static int Add(int a, int b)
+        {
+            return(a + b);
+        }
+
+        internal static bool IsMajeur(int age)
+        {
+            if (age < 18)
+            {
+                if (age < 0)
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                     return false;
+                }
+            }
+            else if (age > 140)
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                return true;
+            }   
+        }
+
+        internal static bool IsEven(int a)
+        {
+            if(a % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        internal static bool IsDivisible(int a, int b)
+        {
+
+
+            if (b == 0)
+            {
+                return false;
+            }
+            else if (a % b == 1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        internal static bool IsPrimary(int a)
+        {
+            int p = 0;
+            for(int i = 1; i <= a; i++)
+            {
+                if (a % i == 0)
+                {
+                    p++;
+                }
+            }
+            if(p == 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        internal static List<int> GetAllPrimary(int a)
+        {
+            List<int> nbr = new List<int>();
+            while (a != 0)
+            {
+                if(IsPrimary(a))
+                {
+                    nbr.Add(a);
+                }
+                a--;
+            }
+            return nbr;
+        }
+
+        internal static int Power2(int a)
+        {
+            return Power(a, 2);
+        }
+
+        internal static int Power(int a, int b)
+        {
+            int expo = 1;
+            for (int i=0; i < b; i++)
+            {
+
+                expo *= a;
+                   
+                    
+            }
+            return expo;
+        }
+
+        internal static int IsInOrder(int a, int b)
+        {
+            if(a < b)
+            {
+                return 1;
+            }
+            else if(a == b)
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        internal static bool IsListInOrder(List<int> list)
+        {
+            for(int i = 0; i < list.Count - 1; i++)
+            {
+                if(list[i] > list[i + 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        internal static List<int> Sort(List<int> toSort)
+        {
+            List<int> new_List = new List<int>();
+            while(toSort != new_List)
+            {
+                int min = toSort[0];
+                foreach(var x in toSort)
+                {
+                    if(x < min)
+                    {
+                        min = x;
+                    }
+                }
+                new_List.Append(min);
+                toSort.Remove(min);
+            }
+            return new_List;
+        }
+
+        internal static List<int> GenericSort(List<int> toSort, Func<int, int, int> isInOrder)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static int IsInOrderDesc(int arg1, int arg2)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- TU Challenge/MyStringImplementation.cs
+++ TU Challenge/MyStringImplementation.cs
@@ -8,6 +8,72 @@ namespace TU_Challenge
 {
     public class MyStringImplementation
     {
-        
+        internal static bool IsNullEmptyOrWhiteSpace(string input)
+        {
+            if (input == null) return true;
+            foreach (var c in input)
+            {
+                if (c != ' ')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        internal static string MixString(string a, string b)
+        {
+            int destination = 0;
+            if (a.Length < b.Length)
+            {
+                destination = b.Length;
+            }
+            else
+            {
+                destination = a.Length;
+            } 
+
+            string mix = "";
+            for(int i = 0; i < destination; i++)
+            {
+                if(a.Length < b.Length)
+                {
+                    
+                }
+
+                if(a.Length == b.Length)
+                {
+                    mix =  a[i] + b[i];
+                    a++;
+                    b++;
+                }
+            }
+            return mix;
+        }
+
+        internal static string ToLowerCase(string a)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static string Voyelles(string a)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static string BazardString(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static string UnBazardString(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static string ToCesarCode(string input, int offset)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
